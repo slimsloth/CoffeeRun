4499590b2a092d40d85c0d6c8b5b87eea5603447
Sean <smitchell36@csu.fullerton.edu>
2021-02-24 23:36:42 -0800

    first commit

---
+++ formhandler.js
@@ -0,0 +1,28 @@
+(function (window) {
+  "use strict";
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function FormHandler() {
+    if (!selector) {
+      throw new Error("No selector provided");
+    }
+  }
+  this.$formElement = $(selector);
+  if(this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+  }
+
+  FormHandler.prototype.addSubmitHandler = function () {
+      console.log('Setting submit handler for form');
+      this.$formElement.on('submit', function(event){
+          event.preventDefault();
+
+          var data = $(this).serializeArray();
+          console.log(data);
+      });
+  };
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+})(window);
